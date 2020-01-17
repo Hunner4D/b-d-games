0c052cd2e578d279db806ebe9feb7e4a11878831
Hunner <h.herrera.academics@gmail.com>
2020-01-16 18:03:16 -0800

    Small changes to main.js file, prep for game

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,21 @@
 console.log("Up and running!");
 
-let cardOne = "Queen";
-let cardTwo = "Queen";
-let cardThree = "King";
-let cardFour = "King";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay =[];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen")
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king")
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("Congrats! You found a match!");
+	}
+	else {
+		alert("Sorry, try harder next time.");
+	}
+}
