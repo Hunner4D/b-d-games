e60ba582d1014ed283e8cf40b6525b6594146590
Hunner <h.herrera.academics@gmail.com>
2020-01-19 13:50:40 -0800

    Further changes to foundation of JS for game functionality

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -3,19 +3,25 @@ console.log("Up and running!");
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay =[];
 
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen")
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king")
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("Congrats! You found a match!");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1] && cardsInPlay.length === 2) {
+  		console.log("You found a match!");
+	} else if (cardsInPlay[0] !== cardsInPlay[1] && cardsInPlay.length === 2) {
+  		console.log("Sorry, try again.");
 	}
 	else {
-		alert("Sorry, try harder next time.");
+		console.log("Pick a card.")
 	}
 }
+
+function flipCard(cardId) {
+	checkForMatch();
+
+	console.log(cards[cardId] + " was flipped over.");
+	cardsInPlay.push(cards[cardId]);
+}
+
+flipCard(0);
+flipCard(1);
+checkForMatch();
+
