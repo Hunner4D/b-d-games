d7f60d7ede1f55223273cf0930c17b2261e758b3
Hunner <h.herrera.academics@gmail.com>
2020-01-19 15:53:05 -0800

    replaced arrays with objects to group and hold more information

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,13 +1,30 @@
 console.log("Up and running!");
 
-let cards = ["queen", "queen", "king", "king"];
+let cards = [ {
+		rank: "Queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"},
+	{
+		rank: "Queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"},
+	{
+		rank: "King",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"},
+	{
+		rank: "King",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"}
+	];
+
 let cardsInPlay =[];
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1] && cardsInPlay.length === 2) {
   		console.log("You found a match!");
 	} else if (cardsInPlay[0] !== cardsInPlay[1] && cardsInPlay.length === 2) {
-  		console.log("Sorry, try again.");
+  		console.log("No match, try again.");
 	}
 	else {
 		console.log("Pick a card.")
@@ -17,11 +34,11 @@ function checkForMatch() {
 function flipCard(cardId) {
 	checkForMatch();
 
-	console.log(cards[cardId] + " was flipped over.");
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].rank + " of " + cards[cardId].suit + " was chosen.----");
+	console.log(cards[cardId].cardImage);
 }
 
 flipCard(0);
 flipCard(1);
 checkForMatch();
-
